2ff7a5dec50fff3f4d0b8ff4a57f1aff0144bd04
Rita <94860684+ritullia@users.noreply.github.com>
2022-07-21 14:36:13 +0300

    Syle, ganefunction commit

--- src/Components/Cell.jsx
+++ src/Components/Cell.jsx
@@ -1,33 +1,121 @@
 import React from 'react';
 import Square from './Square';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { GameFunction } from '../GameFunction';
 
 function Cell() {
     const [cell, setCell] = useState(['', '', '', '', '', '', '', '', '']);
+
+    const [gamePlayer, setGamePlayer] = useState('0');
+    const [result, setResult] = useState({ winner: 'none', state: 'none' });
+
+    useEffect(() => {
+        checkWinner();
+
+        if (gamePlayer === 'X') {
+            setGamePlayer('O');
+        } else {
+            setGamePlayer('X');
+        }
+    }, [cell]);
+
+    useEffect(() => {
+        if (result.state !== 'none') {
+            alert(`Žaidimas baigtas! Laimėjo: ${result.winner}`);
+        }
+    }, [result]);
+
+    const takeSquare = (sqr) => {
+        setCell(
+            cell.map((value, i) => {
+                if (i === sqr && value === '') {
+                    return gamePlayer;
+                }
+                return value;
+            })
+        );
+    };
+
+    const checkWinner = () => {
+        GameFunction.forEach((currentGameFunc) => {
+            const firstGamePlayer = cell[currentGameFunc[0]];
+
+            if (firstGamePlayer === '') return;
+
+            let winningTemplate = true;
+            currentGameFunc.forEach((i) => {
+                if (cell[i] !== firstGamePlayer) {
+                    winningTemplate = false;
+                }
+            });
+            if (winningTemplate) {
+                setResult({ winner: gamePlayer, state: 'Won!' });
+            }
+        });
+    };
+
     return (
         <div className="cell-board">
             <div className="cell">
                 <Square
                     value={cell[0]}
                     takeSquare={() => {
-                        alert(0);
+                        takeSquare(0);
                     }}
                 ></Square>
                 <Square
                     value={cell[1]}
                     takeSquare={() => {
-                        alert(0);
+                        takeSquare(1);
                     }}
                 ></Square>
                 <Square
                     value={cell[2]}
                     takeSquare={() => {
-                        alert(0);
+                        takeSquare(2);
                     }}
                 ></Square>
             </div>
-            <div className="cell"></div>
-            <div className="cell"></div>
+            <div className="cell">
+                <Square
+                    value={cell[3]}
+                    takeSquare={() => {
+                        takeSquare(3);
+                    }}
+                ></Square>
+                <Square
+                    value={cell[4]}
+                    takeSquare={() => {
+                        takeSquare(4);
+                    }}
+                ></Square>
+                <Square
+                    value={cell[5]}
+                    takeSquare={() => {
+                        takeSquare(5);
+                    }}
+                ></Square>
+            </div>
+            <div className="cell">
+                <Square
+                    value={cell[6]}
+                    takeSquare={() => {
+                        takeSquare(6);
+                    }}
+                ></Square>
+                <Square
+                    value={cell[7]}
+                    takeSquare={() => {
+                        takeSquare(7);
+                    }}
+                ></Square>
+                <Square
+                    value={cell[8]}
+                    takeSquare={() => {
+                        takeSquare(8);
+                    }}
+                ></Square>
+            </div>
         </div>
     );
 }
