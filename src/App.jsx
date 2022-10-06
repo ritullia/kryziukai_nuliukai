9c0b8381791395e3239e314a8edeea265b3c92ae
Rita <94860684+ritullia@users.noreply.github.com>
2022-10-06 14:53:24 +0300

    player commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,11 @@ import Cell from './Components/Cell';
 function App() {
     return (
         <div className="game">
-            <h1>Žaidimas /Kryžiukai-nuliukai/</h1>
+            <h1>Kryžiukai - nuliukai</h1>
+            <h1>Pažaiskime!!!</h1>
             <Cell></Cell>
+            <h2 className='player'>Žaidėjas 1:</h2>
+            <h2 className='player'>Žaidėjas 2:</h2>
         </div>
     );
 }
